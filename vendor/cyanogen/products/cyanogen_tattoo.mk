f340824a34026623138b5e4be7f9da172348c4cc
calimochoazucarado <calimochoazucarado@gmail.com>
2011-01-12 08:57:49 +0100

    Modified vendor product_tatto

--- vendor/cyanogen/products/cyanogen_tattoo.mk
+++ vendor/cyanogen/products/cyanogen_tattoo.mk
@@ -1,3 +1,4 @@
+# Inherit AOSP device configuration for Tattoo.
 $(call inherit-product, device/htc/tattoo/tattoo.mk)
 
 # Inherit some common cyanogenmod stuff.
@@ -6,24 +7,41 @@ $(call inherit-product, vendor/cyanogen/products/common.mk)
 # Include GSM stuff
 $(call inherit-product, vendor/cyanogen/products/gsm.mk)
 
+# Build kernel
+PRODUCT_SPECIFIC_DEFINES += TARGET_PREBUILT_KERNEL=device/htc/tattoo/kernel
+
+#
 # Setup device specific product configuration.
+#
 PRODUCT_NAME := cyanogen_tattoo
-PRODUCT_BRAND := htc
+PRODUCT_BRAND := htc_wwe
 PRODUCT_DEVICE := tattoo
-PRODUCT_MODEL := HTC Tattoo
+PRODUCT_MODEL := HTC tattoo
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_tattoo BUILD_ID=FRG83 BUILD_DISPLAY_ID=GRH78 BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.2.1/FRG83/60505:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.2.1 FRG83 60505 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=FRG83 BUILD_DISPLAY_ID=GRH78 BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.2.1/FRG83/60505:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.2.1 FRG83 60505 release-keys"
+
+# Extra Passion overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/tattoo
+
+# Extra RIL settings
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.ril.enable.managed.roaming=1 \
+    ro.ril.oem.nosim.ecclist=911,112,113,115,117,999,000,08,118,120,122,110,119,995 \
+    ro.ril.emc.mode=2
 
 # Enable Windows Media
 WITH_WINDOWS_MEDIA := true
 
-# Extra Tattoo overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/tattoo
-
-# Set ro.modversion
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.modversion=Kalim-10.0
-
-# Copy tattoo specific prebuilt files
 PRODUCT_COPY_FILES +=  \
-    device/htc/tattoo/custom/bootanimation.zip:system/media/bootanimation.zip
+    vendor/cyanogen/prebuilt/ldpi/media/bootanimation.zip:system/media/bootanimation.zip
+
+#
+# Set ro.modversion
+#
+ifdef CYANOGEN_NIGHTLY
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.modversion=KalimAz-10.0_CyanogenMod-7-$(shell date +%m%d%Y)-NIGHTLY-tattoo
+else
+    PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=KalimAz-10.0_CyanogenMod-7.0.0-RC0-tattoo
+endif
