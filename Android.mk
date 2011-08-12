bfe1d61f54c099182846ffab509b4bea33501bbd
Diogo Ferreira <defer@cyanogenmod.com>
2011-08-13 00:01:13 +0100

    click: Fix build breakage for other devices
    Change-Id: I29f5508455a5b177cfc3c260ca248d075ccd2baf

--- Android.mk
+++ Android.mk
@@ -13,11 +13,14 @@
 # limitations under the License.
 
 LOCAL_PATH := $(my-dir)
-subdir_makefiles := \
+
+ifeq ($(TARGET_DEVICE),click)
+    subdir_makefiles := \
         $(LOCAL_PATH)/libcopybit/Android.mk \
         $(LOCAL_PATH)/libgralloc/Android.mk \
         $(LOCAL_PATH)/liblights/Android.mk \
         $(LOCAL_PATH)/libcamera/Android.mk \
         $(LOCAL_PATH)/libsensors/Android.mk
 
-include $(subdir_makefiles)
+    include $(subdir_makefiles)
+endif
